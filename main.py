e5897df41dd0e12e6f26f6aec1d13f274d66867f
Mohamed Meerasha <mohamedmeerasham.cse2024@citchennai.net>
2026-06-24 19:07:53 +0530

    cors error

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Tighten for production
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
